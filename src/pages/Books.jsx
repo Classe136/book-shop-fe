56c82d360e7516dece7fc22c490ee92656c47ed5
klelia <clelia.fradella@gmail.com>
2025-01-29 13:26:15 +0100

    pagination

--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -10,13 +10,19 @@ const bookEndPoint = "/books";
 export const Books = () => {
   const [books, setBooks] = useState([]);
   const { setIsLoading } = useGlobalContext();
-  useEffect(getData, []);
-
+  const [numPages, setNumPages] = useState(0);
+  const [page, setPage] = useState(1);
+  useEffect(getData, [page]);
+  function handleClick(page) {
+    console.log(page);
+    setPage(page);
+  }
   function getData() {
     setIsLoading(true);
     axios
-      .get(`${apiUrl}${bookEndPoint}`)
+      .get(`${apiUrl}${bookEndPoint}`, { params: { page } })
       .then((res) => {
+        setNumPages(Math.ceil(res.data.count / 2));
         setBooks(res.data.items);
       })
       .catch((error) => {
@@ -37,6 +43,40 @@ export const Books = () => {
             <CardComponent data={book} />
           </div>
         ))}
+        <nav className="col-12">
+          <ul className="pagination">
+            <li className={`page-item ${page <= 1 ? "disabled" : ""}`}>
+              <button
+                className="page-link"
+                onClick={() => handleClick(page - 1)}
+              >
+                Previous
+              </button>
+            </li>
+            {new Array(numPages).fill("").map((num, index) => (
+              <li
+                className={`page-item ${page === index + 1 ? "active" : ""} `}
+                aria-current="page"
+                key={index + 1}
+              >
+                <button
+                  className="page-link"
+                  onClick={() => handleClick(index + 1)}
+                >
+                  {index + 1}
+                </button>
+              </li>
+            ))}
+            <li className={`page-item ${page >= numPages ? "disabled" : ""}`}>
+              <button
+                className="page-link"
+                onClick={() => handleClick(page + 1)}
+              >
+                Next
+              </button>
+            </li>
+          </ul>
+        </nav>
       </div>
     </section>
   );
